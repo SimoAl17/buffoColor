679103181ed696fadc7dd364d2055d61271d1c90
SimoAl17 <simo.al.17w@gmail.com>
2022-04-01 13:28:13 +0200

    added real functions

--- buffoColor.js
+++ buffoColor.js
@@ -1,4 +1,66 @@
+/**
+ * Set the opacity of a hex color
+ * @param hexColor - the hex color code without the alpha value.
+ * @param opacity - The opacity of the color. This is a value between 0 and 1.
+ * @returns The hex color with the alpha value applied.
+ */
+export function setOpacity(hexColor, opacity) {
+    const alphaOpacity = Math.round(opacity * 255);
+    const hexOpacity = alphaOpacity.toString(16).toUpperCase();
+    return hexColor + hexOpacity;
+}
 
-export function sayHello() {
-    console.log('BUFFO!');
+/**
+ * Convert a color string to an RGBA string
+ * @param colorString - The color string to convert to RGBA.
+ * @returns The RGBA values of the color.
+ */
+export function fromColorStringToRGBA(colorString) {
+    let r = colorString.substring(1, 3);
+    let g = colorString.substring(3, 5);
+    let b = colorString.substring(5, 7);
+    let a = colorString.substring(7, 9);
+    r = parseInt(r, 16);
+    g = parseInt(g, 16);
+    b = parseInt(b, 16);
+    a = parseInt(a, 16);
+    return r + ',' + g + ',' + b + ',' + a;
+}
+
+
+/**
+ * Given a hex color, return the color that would be the opposite of that color
+ * @param hexColor - The hex color code to convert to a contrasting color.
+ * @returns a hex color code with the opposite color.
+ */
+export function getContrastingColor(hexColor) {
+    let r = hexColor.substring(1, 3);
+    let g = hexColor.substring(3, 5);
+    let b = hexColor.substring(5, 7);
+    let a = hexColor.substring(7, 9);
+    r = parseInt(r, 16);
+    g = parseInt(g, 16);
+    b = parseInt(b, 16);
+    a = parseInt(a, 16);
+    r = 255 - r;
+    g = 255 - g;
+    b = 255 - b;
+    a = 255 - a;
+    r = r.toString(16).toUpperCase();
+    g = g.toString(16).toUpperCase();
+    b = b.toString(16).toUpperCase();
+    a = a.toString(16).toUpperCase();
+    if (r.toString().length === 1) {
+        r = '0' + r;
+    }
+    if (g.toString().length === 1) {
+        g = '0' + g;
+    }
+    if (b.toString().length === 1) {
+        b = '0' + b;
+    }
+    if (a.toString().length === 1) {
+        a = '0' + a;
+    }
+    return '#' + r + g + b + a;
 }
---
+++ readme.md
@@ -0,0 +1,51 @@
+## Functions
+
+<dl>
+<dt><a href="#setOpacity">setOpacity(hexColor, opacity)</a></dt>
+<dd><p>Set the opacity of a hex color</p>
+</dd>
+<dt><a href="#fromColorStringToRGBA">fromColorStringToRGBA(colorString)</a></dt>
+<dd><p>Convert a color string to an RGBA string</p>
+</dd>
+<dt><a href="#getContrastingColor">getContrastingColor(hexColor)</a></dt>
+<dd><p>Given a hex color, return the color that would be the opposite of that color</p>
+</dd>
+</dl>
+
+<a name="setOpacity"></a>
+
+## setOpacity(hexColor, opacity)
+Set the opacity of a hex color
+
+**Kind**: global function  
+**Returns**: The hex color with the alpha value applied.  
+
+| Param | Description |
+| --- | --- |
+| hexColor | the hex color code without the alpha value. |
+| opacity | The opacity of the color. This is a value between 0 and 1. |
+
+<a name="fromColorStringToRGBA"></a>
+
+## fromColorStringToRGBA(colorString)
+Convert a color string to an RGBA string
+
+**Kind**: global function  
+**Returns**: The RGBA values of the color.  
+
+| Param | Description |
+| --- | --- |
+| colorString | The color string to convert to RGBA. |
+
+<a name="getContrastingColor"></a>
+
+## getContrastingColor(hexColor)
+Given a hex color, return the color that would be the opposite of that color
+
+**Kind**: global function  
+**Returns**: a hex color code with the opposite color.  
+
+| Param | Description |
+| --- | --- |
+| hexColor | The hex color code to convert to a contrasting color. |
+
